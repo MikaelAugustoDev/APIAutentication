6b6093cc173be978b2631c16e27b8f9fba04606c
Mikael Augusto <mikaelaugustodev@gmail.com>
2023-05-30 10:19:35 -0300

    Instalando o Express

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,12 @@
-// asdasdasdasdasd
-const teste = "asdasd";
-console.log(teste);
+import express from "express";
+
+const port = 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Home Page");
+});
+
+app.listen(port, () => {
+    console.log("Servidos rodando em http://localhost:3000")
+})
